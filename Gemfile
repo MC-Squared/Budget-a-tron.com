921b00caecc51928031276356751786c3800211e
J Miles <j.milesy.nz@gmail.com>
2018-04-12 13:51:24 +1200

    Add FactoryBot and basic homepage nav link spec

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :development, :test do
   gem 'pry-rails', '~> 0.3.6'
   gem 'rspec-rails', '~> 3.7', '>= 3.7.2'
   gem 'capybara', '~> 3.0', '>= 3.0.1'
+  gem 'factory_bot_rails', '~> 4.8', '>= 4.8.2'
 end
 
 group :development do
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,9 +1,35 @@
 require 'rails_helper'
 
 describe 'Homepage' do
-  it "can be reached successfully" do
+  before do
     visit root_path
+  end
+
+  it "can be reached successfully" do
     expect(page).to have_http_status(:success)
   end
 
+  it "shows login and register to guest users" do
+    expect(page).to have_selector('nav .login')
+    expect(page).to have_selector('nav .register')
+  end
+
+  it "hides logout from guest users" do
+    expect(page).to_not have_selector('nav .logout')
+  end
+
+  it "shows logout to logged in users" do
+    user = FactoryBot.create(:user)
+    login_as(user)
+    visit root_path
+    expect(page).to have_selector('nav .logout')
+  end
+
+  it "hides login and register from logged in users" do
+    user = FactoryBot.create(:user)
+    login_as(user)
+    visit root_path
+    expect(page).to_not have_selector('nav .login')
+    expect(page).to_not have_selector('nav .register')
+  end
 end
