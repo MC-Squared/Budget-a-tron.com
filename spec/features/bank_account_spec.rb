31677a9ff5fb5db79dad924ed90b395ad0a44a24
J Miles <j.milesy.nz@gmail.com>
2018-04-19 21:28:38 +1200

    Tidy up bank account and bank transaction specs

--- spec/features/bank_account_spec.rb
+++ spec/features/bank_account_spec.rb
@@ -1,221 +1,186 @@
 require 'rails_helper'
 
-describe 'redirection' do
-  describe 'index' do
-    it 'does not allow access without being signed in' do
-      visit bank_accounts_path
-      expect(current_path).to eq(new_user_session_path)
-    end
-  end
-
-  describe 'show' do
-    it 'does not allow access without being signed in' do
-      account = FactoryBot.create(:bank_account)
-      visit bank_account_path(account)
-      expect(current_path).to eq(new_user_session_path)
-    end
-  end
-
-  describe 'new' do
-    it 'does not allow access without being signed in' do
-      visit new_bank_account_path
-      expect(current_path).to eq(new_user_session_path)
-    end
-  end
-
-  describe 'edit' do
-    it 'does not allow access without being signed in' do
-      account = FactoryBot.create(:bank_account)
-      visit edit_bank_account_path(account)
-      expect(current_path).to eq(new_user_session_path)
-    end
-  end
-
-end
-
-describe 'navigate' do
+describe 'BankAccount' do
   let(:user) do
     FactoryBot.create(:user)
   end
 
-  describe 'index' do
-    before do
-      login_as user
+  let(:account) do
+    FactoryBot.create(:bank_account, user: user)
+  end
+
+  describe 'authorization' do
+    it 'does not allow access without being signed in' do
       visit bank_accounts_path
-    end
-
-    it 'can be reached successfully' do
-      expect(page.status_code).to eq(200)
-    end
-
-    it 'has a tile of Bank Accounts' do
-      expect(page).to have_content(/Overview/)
-    end
-
-    it 'has a list of bank accounts' do
-      bank_account = FactoryBot.create(:bank_account)
-      bank_account.update!(user: user)
-      second_account = FactoryBot.create(:second_bank_account)
-      second_account.update!(user: user)
-
-      visit bank_accounts_path
-      expect(page).to have_text bank_account.name
-      expect(page).to have_text second_account.name
-    end
-
-    it 'has a scope so that users can only see their own accounts' do
-      other_user = FactoryBot.create(:second_user)
-      other_user_account = BankAccount.create!(
-                                        user: other_user,
-                                        name: 'should be hidden',
-                                        bank_number: 'dont see me',
-                                        start_balance: 0
-                                      )
-
-      visit bank_accounts_path
-      expect(page).to_not have_content(/should be hidden/)
-    end
-
-    it 'has a link to the new page' do
-      click_link('new_bank_account')
-      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(new_user_session_path)
     end
   end
 
-  describe 'show' do
-    let(:account) do
-      account = FactoryBot.create(:bank_account)
-      account.update!(user: user)
-      account
+  describe 'navigate' do
+    describe 'index' do
+      before do
+        login_as user
+        visit bank_accounts_path
+      end
+
+      it 'can be reached successfully' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'has a tile of Bank Accounts' do
+        expect(page).to have_content(/Overview/)
+      end
+
+      it 'has a list of bank accounts' do
+        bank_account = FactoryBot.create(:bank_account)
+        bank_account.update!(user: user)
+        second_account = FactoryBot.create(:second_bank_account)
+        second_account.update!(user: user)
+
+        visit bank_accounts_path
+        expect(page).to have_text bank_account.name
+        expect(page).to have_text second_account.name
+      end
+
+      it 'has a scope so that users can only see their own accounts' do
+        other_user = FactoryBot.create(:second_user)
+        other_user_account = BankAccount.create!(
+                                          user: other_user,
+                                          name: 'should be hidden',
+                                          bank_number: 'dont see me',
+                                          start_balance: 0
+                                        )
+
+        visit bank_accounts_path
+        expect(page).to_not have_content(/should be hidden/)
+      end
+
+      it 'has a link to the new page' do
+        click_link('new_bank_account')
+        expect(page.status_code).to eq(200)
+      end
     end
 
-    before do
-      account
-      login_as user
-      visit bank_account_path(account)
+    describe 'show' do
+      before do
+        login_as user
+        visit bank_account_path(account)
+      end
+
+      it 'show account information' do
+        expect(page).to have_content(account.name)
+      end
+
+      it 'has a link to the edit page' do
+        click_link('edit_bank_account')
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'cannot be viewed by a non authorized user' do
+        logout(:user)
+        login_as(FactoryBot.create(:second_user))
+
+        visit bank_account_path(account)
+
+        expect(current_path).to eq(root_path)
+      end
     end
 
-    it 'show account information' do
-      expect(page).to have_content(account.name)
+    describe 'creation' do
+      before do
+        login_as user
+        visit new_bank_account_path
+      end
+
+      it 'has a new form that can be reached' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'can be created from new form page' do
+        fill_in 'bank_account[name]', with: 'Test Account Name'
+        fill_in 'bank_account[bank_number]', with: '123-test-456'
+        fill_in 'bank_account[start_balance]', with: 123
+
+        expect { click_on 'Save' }.to change(BankAccount, :count).by(1)
+      end
+
+      it 'will have a user associated with it' do
+        fill_in 'bank_account[name]', with: 'Test User Assoc'
+        fill_in 'bank_account[bank_number]', with: '123-test-456'
+        fill_in 'bank_account[start_balance]', with: 456
+        click_on 'Save'
+
+        expect(user.bank_accounts.last.name).to eq('Test User Assoc')
+      end
     end
 
-    it 'has a link to the edit page' do
-      click_link('edit_bank_account')
-      expect(page.status_code).to eq(200)
+    describe 'edit' do
+      before do
+        login_as user
+        visit edit_bank_account_path(account)
+      end
+
+      it 'has an edit form that can be reached' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'can be edited from edit form page' do
+        fill_in 'bank_account[name]', with: 'Edited Account Name'
+        click_on 'Save'
+
+        expect(page).to have_content(/Edited Account Name/)
+      end
+
+      it 'cannot be edited by a non authorized user' do
+        logout(:user)
+        login_as(FactoryBot.create(:second_user))
+
+        visit edit_bank_account_path(account)
+
+        expect(current_path).to eq(root_path)
+      end
     end
 
-    it 'cannot be viewed by a non authorized user' do
-      logout(:user)
-      login_as(FactoryBot.create(:second_user))
+    describe 'delete' do
+      it 'can be deleted' do
+        logout(:user)
 
-      visit bank_account_path(account)
+        delete_user = FactoryBot.create(:user)
+        login_as(delete_user, :scope => :user)
 
-      expect(current_path).to eq(root_path)
-    end
-  end
+        bank_account_to_delete = BankAccount.create!(
+          name: 'name',
+          bank_number: 'asdf',
+          user: delete_user,
+          start_balance: 0
+        )
 
-  describe 'creation' do
-    before do
-      login_as user
-      visit new_bank_account_path
-    end
+        visit edit_bank_account_path(bank_account_to_delete)
 
-    it 'has a new form that can be reached' do
-      expect(page.status_code).to eq(200)
-    end
+        expect { click_on 'delete_bank_account' }.to change(BankAccount, :count).by(-1)
+        expect(page.status_code).to eq(200)
+      end
 
-    it 'can be created from new form page' do
-      fill_in 'bank_account[name]', with: 'Test Account Name'
-      fill_in 'bank_account[bank_number]', with: '123-test-456'
-      fill_in 'bank_account[start_balance]', with: 123
+      it 'can only be deleted by owner' do
+        logout(:user)
 
-      expect { click_on 'Save' }.to change(BankAccount, :count).by(1)
-    end
+        delete_user = FactoryBot.create(:user)
+        login_as(delete_user)
 
-    it 'will have a user associated with it' do
-      fill_in 'bank_account[name]', with: 'Test User Assoc'
-      fill_in 'bank_account[bank_number]', with: '123-test-456'
-      fill_in 'bank_account[start_balance]', with: 456
-      click_on 'Save'
+        bank_account_to_delete = BankAccount.create!(
+          name: 'name',
+          bank_number: 'asdf',
+          user: delete_user,
+          start_balance: 0
+        )
 
-      expect(user.bank_accounts.last.name).to eq('Test User Assoc')
-    end
-  end
+        visit edit_bank_account_path(bank_account_to_delete)
 
-  describe 'edit' do
-    let(:account) do
-      account = FactoryBot.create(:bank_account)
-      account.update!(user: user)
-      account
-    end
+        bank_account_to_delete.update!(user: user)
 
-    before do
-      account
-      login_as user
-
-      visit edit_bank_account_path(account)
-    end
-
-    it 'has an edit form that can be reached' do
-      expect(page.status_code).to eq(200)
-    end
-
-    it 'can be edited from edit form page' do
-      fill_in 'bank_account[name]', with: 'Edited Account Name'
-      click_on 'Save'
-
-      expect(page).to have_content(/Edited Account Name/)
-    end
-
-    it 'cannot be edited by a non authorized user' do
-      logout(:user)
-      login_as(FactoryBot.create(:second_user))
-
-      visit edit_bank_account_path(account)
-
-      expect(current_path).to eq(root_path)
-    end
-  end
-
-  describe 'delete' do
-    it 'can be deleted' do
-      logout(:user)
-
-      delete_user = FactoryBot.create(:user)
-      login_as(delete_user, :scope => :user)
-
-      bank_account_to_delete = BankAccount.create!(
-        name: 'name',
-        bank_number: 'asdf',
-        user: delete_user,
-        start_balance: 0
-      )
-
-      visit edit_bank_account_path(bank_account_to_delete)
-
-      expect { click_on 'delete_bank_account' }.to change(BankAccount, :count).by(-1)
-      expect(page.status_code).to eq(200)
-    end
-
-    it 'can only be deleted by owner' do
-      logout(:user)
-
-      delete_user = FactoryBot.create(:user)
-      login_as(delete_user)
-
-      bank_account_to_delete = BankAccount.create!(
-        name: 'name',
-        bank_number: 'asdf',
-        user: delete_user,
-        start_balance: 0
-      )
-
-      visit edit_bank_account_path(bank_account_to_delete)
-
-      bank_account_to_delete.update!(user: user)
-
-      expect { click_on 'delete_bank_account' }.to change(BankAccount, :count).by(0)
-      expect(current_path).to eq(root_path)
+        expect { click_on 'delete_bank_account' }.to change(BankAccount, :count).by(0)
+        expect(current_path).to eq(root_path)
+      end
     end
   end
 end
--- spec/features/bank_transaction_spec.rb
+++ spec/features/bank_transaction_spec.rb
@@ -1,43 +1,6 @@
 require 'rails_helper'
 
-describe 'redirection' do
-  let(:bank_transaction) do
-    FactoryBot.create(:bank_transaction)
-  end
-
-  describe 'index' do
-    it 'does not allow access without being signed in' do
-      visit bank_account_bank_transactions_path(bank_transaction)
-      expect(current_path).to eq(new_user_session_path)
-    end
-  end
-
-  describe 'show' do
-    it 'does not allow access without being signed in' do
-      account = FactoryBot.create(:bank_account)
-      visit bank_account_path(account)
-      expect(current_path).to eq(new_user_session_path)
-    end
-  end
-
-  describe 'new' do
-    it 'does not allow access without being signed in' do
-      visit new_bank_account_path
-      expect(current_path).to eq(new_user_session_path)
-    end
-  end
-
-  describe 'edit' do
-    it 'does not allow access without being signed in' do
-      account = FactoryBot.create(:bank_account)
-      visit edit_bank_account_path(account)
-      expect(current_path).to eq(new_user_session_path)
-    end
-  end
-
-end
-
-describe 'navigate' do
+describe 'BankTransaction' do
   let(:bank_transaction) do
     FactoryBot.create(:bank_transaction)
   end
@@ -46,184 +9,193 @@ describe 'navigate' do
     bank_transaction.bank_account.user
   end
 
-  describe 'index' do
-    before do
-      login_as bank_transaction.bank_account.user
-      visit bank_account_bank_transactions_path(bank_transaction.bank_account)
-    end
-
-    it 'can be reached successfully' do
-      expect(page.status_code).to eq(200)
-    end
-
-    it 'has a tile of Account Transactions' do
-      expect(page).to have_content(/Account Transactions/)
-    end
-
-    it 'has a list of transactions' do
-      bank_account = FactoryBot.create(:bank_account)
-      trans1 = FactoryBot.create(:bank_transaction, bank_account: bank_account)
-      trans2 = FactoryBot.create(:second_bank_transaction, bank_account: bank_account)
-
-      login_as bank_account.user
-      visit bank_account_bank_transactions_path(bank_account)
-      expect(page).to have_text trans1.payee
-      expect(page).to have_text trans2.payee
-    end
-
-    it 'only shows transactions from the given account' do
-      user = bank_transaction.bank_account.user
-      bank_account = FactoryBot.create(:bank_account, user: user)
-      other_account = FactoryBot.create(:second_bank_account, user: user)
-      transaction = BankTransaction.create!(
-                                        bank_account: bank_account,
-                                        payee: 'should be visible',
-                                        amount: 123
-                                      )
-      other_transaction = BankTransaction.create!(
-                                        bank_account: other_account,
-                                        payee: 'dont see me',
-                                        amount: 456
-                                      )
-
-      visit bank_account_bank_transactions_path(bank_account)
-      expect(page).to have_content(transaction.payee)
-      expect(page).to_not have_content(other_transaction.payee)
-    end
-
-    it 'has a link to the new page' do
-      click_link('new_bank_transaction')
-      expect(page.status_code).to eq(200)
+  describe 'authorization' do
+    it 'does not allow access without being signed in' do
+      visit bank_account_bank_transactions_path(bank_transaction)
+      expect(current_path).to eq(new_user_session_path)
     end
   end
 
-  describe 'show' do
-    before do
-      login_as bank_transaction.bank_account.user
-      visit bank_account_bank_transaction_path(bank_transaction.bank_account, bank_transaction)
+  describe 'navigate' do
+    describe 'index' do
+      before do
+        login_as bank_transaction.bank_account.user
+        visit bank_account_bank_transactions_path(bank_transaction.bank_account)
+      end
+
+      it 'can be reached successfully' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'has a tile of Account Transactions' do
+        expect(page).to have_content(/Account Transactions/)
+      end
+
+      it 'has a list of transactions' do
+        bank_account = FactoryBot.create(:bank_account)
+        trans1 = FactoryBot.create(:bank_transaction, bank_account: bank_account)
+        trans2 = FactoryBot.create(:second_bank_transaction, bank_account: bank_account)
+
+        login_as bank_account.user
+        visit bank_account_bank_transactions_path(bank_account)
+        expect(page).to have_text trans1.payee
+        expect(page).to have_text trans2.payee
+      end
+
+      it 'only shows transactions from the given account' do
+        user = bank_transaction.bank_account.user
+        bank_account = FactoryBot.create(:bank_account, user: user)
+        other_account = FactoryBot.create(:second_bank_account, user: user)
+        transaction = BankTransaction.create!(
+                                          bank_account: bank_account,
+                                          payee: 'should be visible',
+                                          amount: 123
+                                        )
+        other_transaction = BankTransaction.create!(
+                                          bank_account: other_account,
+                                          payee: 'dont see me',
+                                          amount: 456
+                                        )
+
+        visit bank_account_bank_transactions_path(bank_account)
+        expect(page).to have_content(transaction.payee)
+        expect(page).to_not have_content(other_transaction.payee)
+      end
+
+      it 'has a link to the new page' do
+        click_link('new_bank_transaction')
+        expect(page.status_code).to eq(200)
+      end
     end
 
-    it 'show transaction information' do
-      expect(page).to have_content(bank_transaction.payee)
+    describe 'show' do
+      before do
+        login_as bank_transaction.bank_account.user
+        visit bank_account_bank_transaction_path(bank_transaction.bank_account, bank_transaction)
+      end
+
+      it 'show transaction information' do
+        expect(page).to have_content(bank_transaction.payee)
+      end
+
+      it 'has a link to the edit page' do
+        click_link('edit_bank_transaction')
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'cannot be viewed by a non authorized user' do
+        logout(:user)
+        login_as(FactoryBot.create(:second_user))
+
+        visit bank_account_bank_transaction_path(bank_transaction.bank_account, bank_transaction)
+
+        expect(current_path).to eq(root_path)
+      end
     end
 
-    it 'has a link to the edit page' do
-      click_link('edit_bank_transaction')
-      expect(page.status_code).to eq(200)
+    describe 'creation' do
+      before do
+        login_as user
+        visit new_bank_account_bank_transaction_path(bank_transaction.bank_account)
+      end
+
+      it 'has a new form that can be reached' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'can be created from new form page' do
+        fill_in 'bank_transaction[payee]', with: 'Test Payee'
+        fill_in 'bank_transaction[amount]', with: '123.456'
+
+        expect { click_on 'Create Bank transaction' }.to change(BankTransaction, :count).by(1)
+      end
+
+      it 'will have a user associated with it' do
+        fill_in 'bank_transaction[payee]', with: 'Test Payee User'
+        fill_in 'bank_transaction[amount]', with: '123.456'
+        click_on 'Create Bank transaction'
+
+        expect(user.bank_transactions.last.payee).to eq('Test Payee User')
+      end
+
+      it 'will have an account associated with it' do
+        fill_in 'bank_transaction[payee]', with: 'Test Payee Account'
+        fill_in 'bank_transaction[amount]', with: '123.456'
+        click_on 'Create Bank transaction'
+
+        expect(user.bank_accounts.last.bank_transactions.last.payee).to eq('Test Payee Account')
+      end
     end
 
-    it 'cannot be viewed by a non authorized user' do
-      logout(:user)
-      login_as(FactoryBot.create(:second_user))
+    describe 'edit' do
+      before do
+        login_as user
+        visit edit_bank_account_bank_transaction_path(bank_transaction.bank_account, bank_transaction)
+      end
 
-      visit bank_account_bank_transaction_path(bank_transaction.bank_account, bank_transaction)
+      it 'has an edit form that can be reached' do
+        expect(page.status_code).to eq(200)
+      end
 
-      expect(current_path).to eq(root_path)
-    end
-  end
+      it 'can be edited from edit form page' do
+        fill_in 'bank_transaction[payee]', with: 'Test Account Payee'
+        fill_in 'bank_transaction[amount]', with: 123.45
+        click_on 'Update Bank transaction'
 
-  describe 'creation' do
-    before do
-      login_as user
-      visit new_bank_account_bank_transaction_path(bank_transaction.bank_account)
-    end
+        expect(page).to have_content(/Test Account Payee/)
+      end
 
-    it 'has a new form that can be reached' do
-      expect(page.status_code).to eq(200)
-    end
+      it 'cannot be edited by a non authorized user' do
+        logout(:user)
+        login_as(FactoryBot.create(:second_user))
 
-    it 'can be created from new form page' do
-      fill_in 'bank_transaction[payee]', with: 'Test Payee'
-      fill_in 'bank_transaction[amount]', with: '123.456'
+        visit edit_bank_account_bank_transaction_path(bank_transaction.bank_account, bank_transaction)
 
-      expect { click_on 'Create Bank transaction' }.to change(BankTransaction, :count).by(1)
-    end
-
-    it 'will have a user associated with it' do
-      fill_in 'bank_transaction[payee]', with: 'Test Payee User'
-      fill_in 'bank_transaction[amount]', with: '123.456'
-      click_on 'Create Bank transaction'
-
-      expect(user.bank_transactions.last.payee).to eq('Test Payee User')
+        expect(current_path).to eq(root_path)
+      end
     end
 
-    it 'will have an account associated with it' do
-      fill_in 'bank_transaction[payee]', with: 'Test Payee Account'
-      fill_in 'bank_transaction[amount]', with: '123.456'
-      click_on 'Create Bank transaction'
+    describe 'delete' do
+      it 'can be deleted' do
+        logout(:user)
 
-      expect(user.bank_accounts.last.bank_transactions.last.payee).to eq('Test Payee Account')
-    end
-  end
+        delete_user = FactoryBot.create(:user)
+        account = FactoryBot.create(:bank_account, user: delete_user)
+        login_as(delete_user, :scope => :user)
 
-  describe 'edit' do
-    before do
-      login_as user
-      visit edit_bank_account_bank_transaction_path(bank_transaction.bank_account, bank_transaction)
-    end
+        transaction_to_delete = BankTransaction.create(
+          payee: 'test payee',
+          amount: 123.99,
+          bank_account: account
+        )
 
-    it 'has an edit form that can be reached' do
-      expect(page.status_code).to eq(200)
-    end
+        visit edit_bank_account_bank_transaction_path(account, transaction_to_delete)
 
-    it 'can be edited from edit form page' do
-      fill_in 'bank_transaction[payee]', with: 'Test Account Payee'
-      fill_in 'bank_transaction[amount]', with: 123.45
-      click_on 'Update Bank transaction'
+        expect { click_on 'delete_bank_transaction' }.to change(BankTransaction, :count).by(-1)
+        expect(page.status_code).to eq(200)
+      end
 
-      expect(page).to have_content(/Test Account Payee/)
-    end
+      it 'can only be deleted by owner' do
+        logout(:user)
 
-    it 'cannot be edited by a non authorized user' do
-      logout(:user)
-      login_as(FactoryBot.create(:second_user))
+        delete_user = FactoryBot.create(:user)
+        account = FactoryBot.create(:bank_account, user: delete_user)
+        login_as(delete_user)
 
-      visit edit_bank_account_bank_transaction_path(bank_transaction.bank_account, bank_transaction)
+        transaction_to_delete = BankTransaction.create(
+          payee: 'test payee',
+          amount: 123.99,
+          bank_account: account
+        )
 
-      expect(current_path).to eq(root_path)
-    end
-  end
+        visit edit_bank_account_bank_transaction_path(account, transaction_to_delete)
 
-  describe 'delete' do
-    it 'can be deleted' do
-      logout(:user)
+        new_account = FactoryBot.create(:bank_account)
+        transaction_to_delete.update!(bank_account: new_account)
 
-      delete_user = FactoryBot.create(:user)
-      account = FactoryBot.create(:bank_account, user: delete_user)
-      login_as(delete_user, :scope => :user)
-
-      transaction_to_delete = BankTransaction.create(
-        payee: 'test payee',
-        amount: 123.99,
-        bank_account: account
-      )
-
-      visit edit_bank_account_bank_transaction_path(account, transaction_to_delete)
-
-      expect { click_on 'delete_bank_transaction' }.to change(BankTransaction, :count).by(-1)
-      expect(page.status_code).to eq(200)
-    end
-
-    it 'can only be deleted by owner' do
-      logout(:user)
-
-      delete_user = FactoryBot.create(:user)
-      account = FactoryBot.create(:bank_account, user: delete_user)
-      login_as(delete_user)
-
-      transaction_to_delete = BankTransaction.create(
-        payee: 'test payee',
-        amount: 123.99,
-        bank_account: account
-      )
-
-      visit edit_bank_account_bank_transaction_path(account, transaction_to_delete)
-
-      new_account = FactoryBot.create(:bank_account)
-      transaction_to_delete.update!(bank_account: new_account)
-
-      expect { click_on 'delete_bank_transaction' }.to change(BankTransaction, :count).by(0)
-      expect(current_path).to eq(root_path)
+        expect { click_on 'delete_bank_transaction' }.to change(BankTransaction, :count).by(0)
+        expect(current_path).to eq(root_path)
+      end
     end
   end
 end
