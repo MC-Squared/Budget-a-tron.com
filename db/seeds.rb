60c33edb99b2efd6bf32e388217d8ab66f6ef8c7
J Miles <j.milesy.nz@gmail.com>
2018-04-20 09:43:16 +1200

    Update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: 'test@user.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', confirmed_at: Time.zone.now)
+
+BankAccount.create!(user: User.first, name: 'test account', bank_number: '1234')
