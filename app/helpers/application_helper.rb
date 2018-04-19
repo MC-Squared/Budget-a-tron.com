7049d53cf6ae95609fafd4b3caf7fbd4299af549
J Miles <j.milesy.nz@gmail.com>
2018-04-20 11:29:19 +1200

    Add nav_bar_brand view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_bar_brand
+    content_tag :span do
+      concat image_tag 'logo-white.svg', class: 'nav-logo'
+      concat 'Budget-a-tron'
+    end
+  end
 end
