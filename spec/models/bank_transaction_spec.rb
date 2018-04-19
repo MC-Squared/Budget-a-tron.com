d10e66e949c223151e55024936fb7b78b3f1a8f5
J Miles <j.milesy.nz@gmail.com>
2018-04-20 09:43:26 +1200

    Tidy up bank transaction spec

--- spec/models/bank_transaction_spec.rb
+++ spec/models/bank_transaction_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe BankTransaction, type: :model do
   describe "creation" do
+    let (:bank_account) do
+      FactoryBot.create(:bank_account)
+    end
+
     it "can be created if valid" do
-      bank_account = FactoryBot.create(:bank_account)
       transaction = BankTransaction.create(
         bank_account: bank_account,
         payee: 'test payee',
@@ -11,13 +14,38 @@ RSpec.describe BankTransaction, type: :model do
       )
       expect(transaction).to be_valid
     end
+
     it "requires an amount" do
-      bank_account = FactoryBot.create(:bank_account)
       transaction = BankTransaction.create(
         bank_account: bank_account,
         payee: 'test payee',
       )
       expect(transaction).to_not be_valid
+    end
+
+    it "calculates the sum per day" do
+      BankTransaction.create(
+        date: Date.today,
+        bank_account: bank_account,
+        amount: 123,
+        payee: 'test payee'
+      )
+      BankTransaction.create(
+        date: Date.today,
+        bank_account: bank_account,
+        amount: 456,
+        payee: 'adsf'
+      )
+      BankTransaction.create(
+        date: Date.today - 1.day,
+        bank_account: bank_account,
+        amount: 1000,
+        payee: 'test'
+      )
+
+      sums = bank_account.bank_transactions.sum_by_day
+      expect(sums[Date.today - 1.day]).to eq(1000)
+      expect(sums[Date.today]).to eq(579)
     end
   end
 end
