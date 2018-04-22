ee234111160dbd52c1d1c2efab835e90eaed1eaf
J Miles <j.milesy.nz@gmail.com>
2018-04-22 22:03:49 +1200

    Add missing dependent deletion rules to category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  let(:user) do
+    FactoryBot.create(:user)
+  end
+
   describe "creation" do
     it "can be created if valid" do
-      user = FactoryBot.create(:user)
       category = Category.create(
         user: user,
         name: 'category',
@@ -12,7 +15,6 @@ RSpec.describe Category, type: :model do
     end
 
     it "must have a unique name for this user" do
-      user = FactoryBot.create(:user)
       category = Category.create(
         user: user,
         name: 'category',
@@ -25,6 +27,28 @@ RSpec.describe Category, type: :model do
       )
 
       expect(category2).to_not be_valid
+    end
+  end
+
+  describe "deletion" do
+    it "deletes child category_rules" do
+      category = FactoryBot.create(:category, user: user)
+
+      category_rule = FactoryBot.create(:category_rule, category: category)
+
+      expect { category.destroy }.to change(Category, :count).by(-1)
+        .and change(CategoryRule, :count).by(-1)
+    end
+
+    it "nullifies child bank transactions" do
+      category = FactoryBot.create(:category, user: user)
+
+      bank_transaction = FactoryBot.create(:bank_transaction, category: category)
+
+      expect { category.destroy }.to change(Category, :count).by(-1)
+        .and change(BankTransaction, :count).by (0)
+
+      expect(BankTransaction.find(bank_transaction.id).category).to eq(nil)
     end
   end
 end
