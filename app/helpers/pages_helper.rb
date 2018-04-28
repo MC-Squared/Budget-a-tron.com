ae234b24b9cf94c7bcc8a65d3ee201bf405b910b
J Miles <j.milesy.nz@gmail.com>
2018-04-28 18:12:19 +1200

    Add 'How to' / 'Getting started' page with links

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,37 @@
 module PagesHelper
+  def howto_list_item(number, verb, link_text, path, done)
+    prefix = "#{number}. #{verb.titleize} "
+    if user_signed_in?
+      if done
+        content_tag :div do
+          concat (content_tag :span, class:'strikethrough' do
+            concat prefix
+            concat link_text
+          end)
+          concat " "
+          concat check_icon
+        end
+      else
+        content_tag :div do
+          concat prefix
+          concat link_to(link_text, path)
+        end
+      end
+    else
+      content_tag :div, prefix + link_text
+    end
+  end
+
+  def howto_list_resource(number, verb, resource, path)
+    resource_text = "#{resource.to_s.humanize.downcase}"
+    done = user_signed_in? && current_user.send(resource).count > 0
+    howto_list_item(number, verb, resource_text, path, done)
+  end
+
+  private
+
+    def check_icon
+      content_tag :span, nil,
+        class:'howto-icon', 'data-feather': 'check-square'
+    end
 end
